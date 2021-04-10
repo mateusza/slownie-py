d9a30c99064921e308b4fce599c456e09048d751
Mateusz Adamowski <mateusz@adamowski.pl>
2021-04-10 15:58:59 +0200

    refactor

--- slownie.py
+++ slownie.py
@@ -1,87 +1,90 @@
-class Slownie:
-    rzedy = (
-            ('', '', ''),
-            ('tysiąc', 'tysiące', 'tysięcy'),
-            ('milion', 'miliony', 'milionów'),
-            ('miliard', 'miliardy', 'miliardów'),
-            ('bilion', 'biliony', 'bilionów'),
-            ('biliard', 'biliardy', 'biliardów'),
-            ('trylion', 'tryliony', 'trylionów'),
-            ('tryliard', 'tryliardy', 'tryliardów'),
-            ('kwadrylion', 'kwadryliony', 'kwadrylionów'),
-            ('kwadryliard', 'kwadryliardy', 'kwadryliardów'),
-            ('kwintylion', 'kwintyliony', 'kwintylionów'),
-            ('kwintyliard', 'kwintyliardy', 'kwintyliardów')
-    )
+#!/usr/bin/env python3
 
-    slowa = (
-            ('', 'jeden', 'dwa', 'trzy', 'cztery', 'pięć',
-             'sześć', 'siedem', 'osiem', 'dziewięć', 'dziesięć',
-             'jedenaście', 'dwanaście', 'trzynaście', 'czternaście', 'piętnaście',
-             'szesnaście', 'siedemnaście', 'osiemnaście', 'dziewiętnaście'),
-            ('', 'dziesięć', 'dwadzieścia', 'trzydzieści', 'czterdzieści',
-             'pięćdziesiąt', 'sześćdziesiąt', 'siedemdziesiąt', 'osiemdziesiąt', 'dziewięćdziesiąt'),
-            ('', 'sto', 'dwieście', 'trzysta', 'czterysta',
-             'pięćset', 'sześćset', 'siedemset', 'osiemset', 'dziewięćset')
-    )
+rzedy = (
+        ('', '', ''),
+        ('tysiąc', 'tysiące', 'tysięcy'),
+        ('milion', 'miliony', 'milionów'),
+        ('miliard', 'miliardy', 'miliardów'),
+        ('bilion', 'biliony', 'bilionów'),
+        ('biliard', 'biliardy', 'biliardów'),
+        ('trylion', 'tryliony', 'trylionów'),
+        ('tryliard', 'tryliardy', 'tryliardów'),
+        ('kwadrylion', 'kwadryliony', 'kwadrylionów'),
+        ('kwadryliard', 'kwadryliardy', 'kwadryliardów'),
+        ('kwintylion', 'kwintyliony', 'kwintylionów'),
+        ('kwintyliard', 'kwintyliardy', 'kwintyliardów')
+)
 
-    @classmethod
-    def grupa(cls, liczba: int, zero: bool = True, wysoka: bool = False) -> str:
-        if type(liczba) != int:
-            raise TypeError
-        if liczba < 0:
-            raise ValueError
-        if liczba > 999:
-            raise ValueError
-        j, d, s = ((liczba//n)%10 for n in (1, 10, 100))
-        if d == 1:
-            d = 0
-            j += 10
-        if liczba == 0 and zero:
-            return 'zero'
-        if liczba == 1 and wysoka:
-            return ''
-        return ' '.join((cls.slowa[n][x] for (n,x) in ((2, s), (1, d), (0, j)) if x > 0))
+slowa = (
+        ('', 'jeden', 'dwa', 'trzy', 'cztery', 'pięć',
+         'sześć', 'siedem', 'osiem', 'dziewięć', 'dziesięć',
+         'jedenaście', 'dwanaście', 'trzynaście', 'czternaście', 'piętnaście',
+         'szesnaście', 'siedemnaście', 'osiemnaście', 'dziewiętnaście'),
+        ('', 'dziesięć', 'dwadzieścia', 'trzydzieści', 'czterdzieści',
+         'pięćdziesiąt', 'sześćdziesiąt', 'siedemdziesiąt', 'osiemdziesiąt', 'dziewięćdziesiąt'),
+        ('', 'sto', 'dwieście', 'trzysta', 'czterysta',
+         'pięćset', 'sześćset', 'siedemset', 'osiemset', 'dziewięćset')
+)
 
-    @staticmethod
-    def odmien(liczba: int, slowa: list) -> str:
-        if liczba < 0:
-            liczba = -liczba
-        try:
-            assert type(slowa) != str
-        except:
-            raise TypeError
-        slowo_jeden, slowo_dwa, slowo_piec = slowa
-        if liczba == 1:
-            return slowo_jeden
-        if liczba % 10 in (2, 3, 4) and liczba % 100 not in range(11, 20):
-            return slowo_dwa
-        return slowo_piec
+def grupa(liczba: int, zero: bool = True, wysoka: bool = False) -> str:
+    if type(liczba) != int:
+        raise TypeError
+    if liczba < 0:
+        raise ValueError
+    if liczba > 999:
+        raise ValueError
+    j, d, s = ((liczba//n)%10 for n in (1, 10, 100))
+    if d == 1:
+        d = 0
+        j += 10
+    if liczba == 0 and zero:
+        return 'zero'
+    if liczba == 1 and wysoka:
+        return ''
+    return ' '.join((slowa[n][x] for (n,x) in ((2, s), (1, d), (0, j)) if x > 0))
 
-    @classmethod
-    def liczba(cls, liczba: int) -> str:
-        try:
-            assert liczba == int(liczba)
-        except:
-            raise TypeError
+def odmien(liczba: int, slowa: list) -> str:
+    if liczba < 0:
+        liczba = -liczba
+    try:
+        assert type(slowa) != str
+    except:
+        raise TypeError
+    slowo_jeden, slowo_dwa, slowo_piec = slowa
+    if liczba == 1:
+        return slowo_jeden
+    if liczba % 10 in (2, 3, 4) and liczba % 100 not in range(11, 20):
+        return slowo_dwa
+    return slowo_piec
 
-        if liczba < 0:
-            return "minus " + str(cls.liczba(-liczba))
-        segmenty = []
-        if liczba == 0:
-            segmenty.append(0)
-        while liczba > 0:
-            segmenty.append(liczba%1000)
-            liczba //= 1000
-        odmienione = [
-            ' '.join([
-                x for x in [
-                    cls.grupa(int(segment), (rzad, len(segmenty)) == (0, 1), rzad > 0),
-                    cls.odmien(segment, cls.rzedy[rzad]) if rzad > 0 and segment != 0 else ''
-                ]
-                if len(x) > 0
-            ])
-            for (rzad, segment) in list(enumerate(segmenty))[::-1]
-        ]
-        return ' '.join([o for o in odmienione if len(o) > 0])
+def slownie(liczba: int) -> str:
+    try:
+        assert liczba == int(liczba)
+    except:
+        raise TypeError
+
+    if liczba < 0:
+        return "minus " + str(slownie(-liczba))
+    segmenty = []
+    if liczba == 0:
+        segmenty.append(0)
+    while liczba > 0:
+        segmenty.append(liczba%1000)
+        liczba //= 1000
+    odmienione = [
+        ' '.join([
+            x for x in [
+                grupa(int(segment), (rzad, len(segmenty)) == (0, 1), rzad > 0),
+                odmien(segment, rzedy[rzad]) if rzad > 0 and segment != 0 else ''
+            ]
+            if len(x) > 0
+        ])
+        for (rzad, segment) in list(enumerate(segmenty))[::-1]
+    ]
+    return ' '.join([o for o in odmienione if len(o) > 0])
+
+if __name__ == '__main__':
+    import sys
+    for arg in sys.argv[1:]:
+        print(slownie(int(arg)))
 
--- test_slownie.py
+++ test_slownie.py
@@ -2,235 +2,235 @@
 
 import pytest
 
-from slownie import Slownie
+import slownie
 
 class Test_Odmien():
     odmiana = ['kot', 'koty', 'kotów']
 
     def test_zakres_0_9(self):
-        assert Slownie.odmien(0, self.odmiana) == 'kotów'
-        assert Slownie.odmien(1, self.odmiana) == 'kot'
-        assert Slownie.odmien(2, self.odmiana) == 'koty'
-        assert Slownie.odmien(3, self.odmiana) == 'koty'
-        assert Slownie.odmien(4, self.odmiana) == 'koty'
-        assert Slownie.odmien(5, self.odmiana) == 'kotów'
-        assert Slownie.odmien(6, self.odmiana) == 'kotów'
+        assert slownie.odmien(0, self.odmiana) == 'kotów'
+        assert slownie.odmien(1, self.odmiana) == 'kot'
+        assert slownie.odmien(2, self.odmiana) == 'koty'
+        assert slownie.odmien(3, self.odmiana) == 'koty'
+        assert slownie.odmien(4, self.odmiana) == 'koty'
+        assert slownie.odmien(5, self.odmiana) == 'kotów'
+        assert slownie.odmien(6, self.odmiana) == 'kotów'
 
     def test_zakres_10_19(self):
-        assert Slownie.odmien(10, self.odmiana) == 'kotów'
-        assert Slownie.odmien(11, self.odmiana) == 'kotów'
-        assert Slownie.odmien(12, self.odmiana) == 'kotów'
-        assert Slownie.odmien(13, self.odmiana) == 'kotów'
-        assert Slownie.odmien(14, self.odmiana) == 'kotów'
-        assert Slownie.odmien(19, self.odmiana) == 'kotów'
+        assert slownie.odmien(10, self.odmiana) == 'kotów'
+        assert slownie.odmien(11, self.odmiana) == 'kotów'
+        assert slownie.odmien(12, self.odmiana) == 'kotów'
+        assert slownie.odmien(13, self.odmiana) == 'kotów'
+        assert slownie.odmien(14, self.odmiana) == 'kotów'
+        assert slownie.odmien(19, self.odmiana) == 'kotów'
 
     def test_zakres_20_99(self):
-        assert Slownie.odmien(20, self.odmiana) == 'kotów'
-        assert Slownie.odmien(21, self.odmiana) == 'kotów'
-        assert Slownie.odmien(22, self.odmiana) == 'koty'
-        assert Slownie.odmien(23, self.odmiana) == 'koty'
-        assert Slownie.odmien(24, self.odmiana) == 'koty'
-        assert Slownie.odmien(25, self.odmiana) == 'kotów'
-        assert Slownie.odmien(29, self.odmiana) == 'kotów'
-        assert Slownie.odmien(30, self.odmiana) == 'kotów'
-        assert Slownie.odmien(31, self.odmiana) == 'kotów'
-        assert Slownie.odmien(32, self.odmiana) == 'koty'
-        assert Slownie.odmien(33, self.odmiana) == 'koty'
-        assert Slownie.odmien(34, self.odmiana) == 'koty'
-        assert Slownie.odmien(35, self.odmiana) == 'kotów'
-        assert Slownie.odmien(39, self.odmiana) == 'kotów'
-        assert Slownie.odmien(40, self.odmiana) == 'kotów'
-        assert Slownie.odmien(41, self.odmiana) == 'kotów'
-        assert Slownie.odmien(42, self.odmiana) == 'koty'
-        assert Slownie.odmien(43, self.odmiana) == 'koty'
-        assert Slownie.odmien(44, self.odmiana) == 'koty'
-        assert Slownie.odmien(45, self.odmiana) == 'kotów'
-        assert Slownie.odmien(49, self.odmiana) == 'kotów'
-        assert Slownie.odmien(99, self.odmiana) == 'kotów'
+        assert slownie.odmien(20, self.odmiana) == 'kotów'
+        assert slownie.odmien(21, self.odmiana) == 'kotów'
+        assert slownie.odmien(22, self.odmiana) == 'koty'
+        assert slownie.odmien(23, self.odmiana) == 'koty'
+        assert slownie.odmien(24, self.odmiana) == 'koty'
+        assert slownie.odmien(25, self.odmiana) == 'kotów'
+        assert slownie.odmien(29, self.odmiana) == 'kotów'
+        assert slownie.odmien(30, self.odmiana) == 'kotów'
+        assert slownie.odmien(31, self.odmiana) == 'kotów'
+        assert slownie.odmien(32, self.odmiana) == 'koty'
+        assert slownie.odmien(33, self.odmiana) == 'koty'
+        assert slownie.odmien(34, self.odmiana) == 'koty'
+        assert slownie.odmien(35, self.odmiana) == 'kotów'
+        assert slownie.odmien(39, self.odmiana) == 'kotów'
+        assert slownie.odmien(40, self.odmiana) == 'kotów'
+        assert slownie.odmien(41, self.odmiana) == 'kotów'
+        assert slownie.odmien(42, self.odmiana) == 'koty'
+        assert slownie.odmien(43, self.odmiana) == 'koty'
+        assert slownie.odmien(44, self.odmiana) == 'koty'
+        assert slownie.odmien(45, self.odmiana) == 'kotów'
+        assert slownie.odmien(49, self.odmiana) == 'kotów'
+        assert slownie.odmien(99, self.odmiana) == 'kotów'
 
     def test_zakres_100_199(self):
-        assert Slownie.odmien(100, self.odmiana) == 'kotów'
-        assert Slownie.odmien(101, self.odmiana) == 'kotów'
-        assert Slownie.odmien(102, self.odmiana) == 'koty'
-        assert Slownie.odmien(103, self.odmiana) == 'koty'
-        assert Slownie.odmien(105, self.odmiana) == 'kotów'
-        assert Slownie.odmien(112, self.odmiana) == 'kotów'
-        assert Slownie.odmien(115, self.odmiana) == 'kotów'
-        assert Slownie.odmien(122, self.odmiana) == 'koty'
-        assert Slownie.odmien(125, self.odmiana) == 'kotów'
+        assert slownie.odmien(100, self.odmiana) == 'kotów'
+        assert slownie.odmien(101, self.odmiana) == 'kotów'
+        assert slownie.odmien(102, self.odmiana) == 'koty'
+        assert slownie.odmien(103, self.odmiana) == 'koty'
+        assert slownie.odmien(105, self.odmiana) == 'kotów'
+        assert slownie.odmien(112, self.odmiana) == 'kotów'
+        assert slownie.odmien(115, self.odmiana) == 'kotów'
+        assert slownie.odmien(122, self.odmiana) == 'koty'
+        assert slownie.odmien(125, self.odmiana) == 'kotów'
 
     def test_zakres_od_200(self):
-        assert Slownie.odmien(200, self.odmiana) == 'kotów'
-        assert Slownie.odmien(201, self.odmiana) == 'kotów'
-        assert Slownie.odmien(202, self.odmiana) == 'koty'
-        assert Slownie.odmien(203, self.odmiana) == 'koty'
-        assert Slownie.odmien(205, self.odmiana) == 'kotów'
-        assert Slownie.odmien(215, self.odmiana) == 'kotów'
-        assert Slownie.odmien(222, self.odmiana) == 'koty'
-        assert Slownie.odmien(905, self.odmiana) == 'kotów'
+        assert slownie.odmien(200, self.odmiana) == 'kotów'
+        assert slownie.odmien(201, self.odmiana) == 'kotów'
+        assert slownie.odmien(202, self.odmiana) == 'koty'
+        assert slownie.odmien(203, self.odmiana) == 'koty'
+        assert slownie.odmien(205, self.odmiana) == 'kotów'
+        assert slownie.odmien(215, self.odmiana) == 'kotów'
+        assert slownie.odmien(222, self.odmiana) == 'koty'
+        assert slownie.odmien(905, self.odmiana) == 'kotów'
 
     def test_str_zamiast_zestawu_slow(self):
         with pytest.raises(TypeError):
-            assert Slownie.odmien(1, 'abc')
+            assert slownie.odmien(1, 'abc')
         with pytest.raises(TypeError):
-            assert Slownie.odmien(1, 'abcdef')
+            assert slownie.odmien(1, 'abcdef')
 
     def test_inny_typ_zamiast_zestawu_slow(self):
-        assert Slownie.odmien(7, ('mysz', 'myszy', 'myszy'))
-        assert Slownie.odmien(9, 'dom domy domów'.split())
+        assert slownie.odmien(7, ('mysz', 'myszy', 'myszy'))
+        assert slownie.odmien(9, 'dom domy domów'.split())
 
         with pytest.raises(TypeError):
-            assert Slownie.odmien(1, 1234)
+            assert slownie.odmien(1, 1234)
         with pytest.raises(TypeError):
-            assert Slownie.odmien(1, bool)
+            assert slownie.odmien(1, bool)
 
     def test_za_krotki_zestaw_slow(self):
 
         with pytest.raises(ValueError):
-            assert Slownie.odmien(1, [])
+            assert slownie.odmien(1, [])
         with pytest.raises(ValueError):
-            assert Slownie.odmien(1, ['pies'])
+            assert slownie.odmien(1, ['pies'])
         with pytest.raises(ValueError):
-            assert Slownie.odmien(1, ['pies', 'psy'])
+            assert slownie.odmien(1, ['pies', 'psy'])
 
     def test_za_dlugi_zestaw_slow(self):
         with pytest.raises(ValueError):
-            assert Slownie.odmien(1, ['pies', 'psy', 'psów', 'psami'])
+            assert slownie.odmien(1, ['pies', 'psy', 'psów', 'psami'])
         with pytest.raises(ValueError):
-            assert Slownie.odmien(1, range(1,10))
+            assert slownie.odmien(1, range(1,10))
 
 class Test_Grupa():
     def test_jednostki(self):
-        assert Slownie.grupa(0) == 'zero'
-        assert Slownie.grupa(1) == 'jeden'
-        assert Slownie.grupa(2) == 'dwa'
-        assert Slownie.grupa(3) == 'trzy'
-        assert Slownie.grupa(4) == 'cztery'
-        assert Slownie.grupa(5) == 'pięć'
-        assert Slownie.grupa(9) == 'dziewięć'
+        assert slownie.grupa(0) == 'zero'
+        assert slownie.grupa(1) == 'jeden'
+        assert slownie.grupa(2) == 'dwa'
+        assert slownie.grupa(3) == 'trzy'
+        assert slownie.grupa(4) == 'cztery'
+        assert slownie.grupa(5) == 'pięć'
+        assert slownie.grupa(9) == 'dziewięć'
 
     def test_jednostki_z_pominietym_zerem(self):
-        assert Slownie.grupa(0, False) == ''
-        assert Slownie.grupa(1, False) == 'jeden'
-        assert Slownie.grupa(2, False) == 'dwa'
-        assert Slownie.grupa(8, False) == 'osiem'
+        assert slownie.grupa(0, False) == ''
+        assert slownie.grupa(1, False) == 'jeden'
+        assert slownie.grupa(2, False) == 'dwa'
+        assert slownie.grupa(8, False) == 'osiem'
 
     def test_jednostki_z_pominietym_zerem_i_jedynka(self):
-        assert Slownie.grupa(0, False, True) == ''
-        assert Slownie.grupa(1, False, True) == ''
-        assert Slownie.grupa(2, False, True) == 'dwa'
+        assert slownie.grupa(0, False, True) == ''
+        assert slownie.grupa(1, False, True) == ''
+        assert slownie.grupa(2, False, True) == 'dwa'
 
     def test_zakres_10_20(self):
-        assert Slownie.grupa(10) == 'dziesięć'
-        assert Slownie.grupa(11) == 'jedenaście'
-        assert Slownie.grupa(12) == 'dwanaście'
-        assert Slownie.grupa(13) == 'trzynaście'
-        assert Slownie.grupa(19) == 'dziewiętnaście'
-        assert Slownie.grupa(20) != 'dziesiętnaście'
+        assert slownie.grupa(10) == 'dziesięć'
+        assert slownie.grupa(11) == 'jedenaście'
+        assert slownie.grupa(12) == 'dwanaście'
+        assert slownie.grupa(13) == 'trzynaście'
+        assert slownie.grupa(19) == 'dziewiętnaście'
+        assert slownie.grupa(20) != 'dziesiętnaście'
 
     def test_zakres_20_99(self):
-        assert Slownie.grupa(20) == 'dwadzieścia'
-        assert Slownie.grupa(21) == 'dwadzieścia jeden'
-        assert Slownie.grupa(22) == 'dwadzieścia dwa'
-        assert Slownie.grupa(32) == 'trzydzieści dwa'
-        assert Slownie.grupa(33) == 'trzydzieści trzy'
-        assert Slownie.grupa(43) == 'czterdzieści trzy'
-        assert Slownie.grupa(53) == 'pięćdziesiąt trzy'
-        assert Slownie.grupa(99) == 'dziewięćdziesiąt dziewięć'
+        assert slownie.grupa(20) == 'dwadzieścia'
+        assert slownie.grupa(21) == 'dwadzieścia jeden'
+        assert slownie.grupa(22) == 'dwadzieścia dwa'
+        assert slownie.grupa(32) == 'trzydzieści dwa'
+        assert slownie.grupa(33) == 'trzydzieści trzy'
+        assert slownie.grupa(43) == 'czterdzieści trzy'
+        assert slownie.grupa(53) == 'pięćdziesiąt trzy'
+        assert slownie.grupa(99) == 'dziewięćdziesiąt dziewięć'
 
     def test_setki(self):
-        assert Slownie.grupa(100) == 'sto'
-        assert Slownie.grupa(200) == 'dwieście'
-        assert Slownie.grupa(300) == 'trzysta'
-        assert Slownie.grupa(400) == 'czterysta'
+        assert slownie.grupa(100) == 'sto'
+        assert slownie.grupa(200) == 'dwieście'
+        assert slownie.grupa(300) == 'trzysta'
+        assert slownie.grupa(400) == 'czterysta'
 
     def test_wartosci_spoza_zakresu(self):
-        assert Slownie.grupa(0)
-        assert Slownie.grupa(1)
-        assert Slownie.grupa(999)
+        assert slownie.grupa(0)
+        assert slownie.grupa(1)
+        assert slownie.grupa(999)
 
         with pytest.raises(ValueError):
-            Slownie.grupa(-1)
+            slownie.grupa(-1)
         with pytest.raises(ValueError):
-            Slownie.grupa(-2)
+            slownie.grupa(-2)
         with pytest.raises(ValueError):
-            Slownie.grupa(-100)
+            slownie.grupa(-100)
         with pytest.raises(ValueError):
-            Slownie.grupa(1000)
+            slownie.grupa(1000)
         with pytest.raises(ValueError):
-            Slownie.grupa(1002)
+            slownie.grupa(1002)
         with pytest.raises(ValueError):
-            Slownie.grupa(1000000)
+            slownie.grupa(1000000)
 
     def test_wartosci_innych_typow(self):
         with pytest.raises(TypeError):
-            Slownie.grupa(-2.5)
+            slownie.grupa(-2.5)
         with pytest.raises(TypeError):
-            Slownie.grupa("123")
+            slownie.grupa("123")
         with pytest.raises(TypeError):
-            Slownie.grupa("qwe")
+            slownie.grupa("qwe")
         with pytest.raises(TypeError):
-            Slownie.grupa([100])
+            slownie.grupa([100])
 
 class Test_Liczba():
     def test_zakres_0_999(self):
-        assert Slownie.liczba(0) == 'zero'
-        assert Slownie.liczba(1) == 'jeden'
-        assert Slownie.liczba(11) == 'jedenaście'
-        assert Slownie.liczba(111) == 'sto jedenaście'
+        assert slownie.slownie(0) == 'zero'
+        assert slownie.slownie(1) == 'jeden'
+        assert slownie.slownie(11) == 'jedenaście'
+        assert slownie.slownie(111) == 'sto jedenaście'
 
     def test_zakres_1000_999999(self):
-        assert Slownie.liczba(1000) != 'jeden tysiąc'
-        assert Slownie.liczba(1000) == 'tysiąc'
-        assert Slownie.liczba(1001) == 'tysiąc jeden'
-        assert Slownie.liczba(1011) == 'tysiąc jedenaście'
-        assert Slownie.liczba(1100) == 'tysiąc sto'
-        assert Slownie.liczba(1111) == 'tysiąc sto jedenaście'
-        assert Slownie.liczba(2000) == 'dwa tysiące'
-        assert Slownie.liczba(2001) == 'dwa tysiące jeden'
-        assert Slownie.liczba(2002) == 'dwa tysiące dwa'
-        assert Slownie.liczba(2003) == 'dwa tysiące trzy'
-        assert Slownie.liczba(2013) == 'dwa tysiące trzynaście'
-        assert Slownie.liczba(5000) == 'pięć tysięcy'
-        assert Slownie.liczba(6000) == 'sześć tysięcy'
-        assert Slownie.liczba(6008) == 'sześć tysięcy osiem'
-        assert Slownie.liczba(12000) == 'dwanaście tysięcy'
-        assert Slownie.liczba(12001) == 'dwanaście tysięcy jeden'
-        assert Slownie.liczba(12010) == 'dwanaście tysięcy dziesięć'
-        assert Slownie.liczba(112033) == 'sto dwanaście tysięcy trzydzieści trzy'
+        assert slownie.slownie(1000) != 'jeden tysiąc'
+        assert slownie.slownie(1000) == 'tysiąc'
+        assert slownie.slownie(1001) == 'tysiąc jeden'
+        assert slownie.slownie(1011) == 'tysiąc jedenaście'
+        assert slownie.slownie(1100) == 'tysiąc sto'
+        assert slownie.slownie(1111) == 'tysiąc sto jedenaście'
+        assert slownie.slownie(2000) == 'dwa tysiące'
+        assert slownie.slownie(2001) == 'dwa tysiące jeden'
+        assert slownie.slownie(2002) == 'dwa tysiące dwa'
+        assert slownie.slownie(2003) == 'dwa tysiące trzy'
+        assert slownie.slownie(2013) == 'dwa tysiące trzynaście'
+        assert slownie.slownie(5000) == 'pięć tysięcy'
+        assert slownie.slownie(6000) == 'sześć tysięcy'
+        assert slownie.slownie(6008) == 'sześć tysięcy osiem'
+        assert slownie.slownie(12000) == 'dwanaście tysięcy'
+        assert slownie.slownie(12001) == 'dwanaście tysięcy jeden'
+        assert slownie.slownie(12010) == 'dwanaście tysięcy dziesięć'
+        assert slownie.slownie(112033) == 'sto dwanaście tysięcy trzydzieści trzy'
 
     def test_ujemne(self):
-        assert Slownie.liczba(-1) == "minus jeden"
-        assert Slownie.liczba(-2) == "minus dwa"
-        assert Slownie.liczba(-10) == "minus dziesięć"
-        assert Slownie.liczba(-100) == "minus sto"
-        assert Slownie.liczba(-1000) == "minus tysiąc"
+        assert slownie.slownie(-1) == "minus jeden"
+        assert slownie.slownie(-2) == "minus dwa"
+        assert slownie.slownie(-10) == "minus dziesięć"
+        assert slownie.slownie(-100) == "minus sto"
+        assert slownie.slownie(-1000) == "minus tysiąc"
 
     def test_wartosci_innych_typow(self):
         from decimal import Decimal as D
         from fractions import Fraction as F
 
         with pytest.raises(TypeError):
-            Slownie.liczba(1234.5)
+            slownie.slownie(1234.5)
         with pytest.raises(TypeError):
-            Slownie.liczba([1234])
+            slownie.slownie([1234])
         with pytest.raises(TypeError):
-            Slownie.liczba("1234")
+            slownie.slownie("1234")
         with pytest.raises(TypeError):
-            Slownie.liczba("qwe")
+            slownie.slownie("qwe")
         with pytest.raises(TypeError):
-            Slownie.liczba(D(10) / D(3))
+            slownie.slownie(D(10) / D(3))
         with pytest.raises(TypeError):
-            Slownie.liczba(F(10) / F(3))
+            slownie.slownie(F(10) / F(3))
         with pytest.raises(TypeError):
-            Slownie.liczba(D(0.8) + D(0.2))
+            slownie.slownie(D(0.8) + D(0.2))
 
-        assert Slownie.liczba(4.0)
-        assert Slownie.liczba(4.5 * 2)
-        assert Slownie.liczba(D(10))
-        assert Slownie.liczba(D(10.0))
-        assert Slownie.liczba(D(10.25) * D(100)) 
-        assert Slownie.liczba(D('10.40') * D(10)) 
-        assert Slownie.liczba(F(100, 4))
-        assert Slownie.liczba(F(100) / F(5)) 
+        assert slownie.slownie(4.0)
+        assert slownie.slownie(4.5 * 2)
+        assert slownie.slownie(D(10))
+        assert slownie.slownie(D(10.0))
+        assert slownie.slownie(D(10.25) * D(100)) 
+        assert slownie.slownie(D('10.40') * D(10)) 
+        assert slownie.slownie(F(100, 4))
+        assert slownie.slownie(F(100) / F(5)) 
 
